2d74535a3249bb628f457ec4ae68b8302a84b3c4
museu-do-novo <138774203+museu-do-novo@users.noreply.github.com>
2025-01-04 11:51:41 -0300

    Update some-iptv-channels-list.sh

--- some-iptv-channels-list.sh
+++ some-iptv-channels-list.sh
@@ -4,7 +4,7 @@
 clear;
 
 # Puxar os links do site
-curl -s https://ip-tv.app/ | grep copy | grep https | awk -F "(" '{print $2}' | awk -F ',' '{print $1}'
+curl -s https://ip-tv.app/ | grep -i "copy" | grep -i "https://" | awk -F '(' '{print $2}' | awk -F ',' '{gsub(/'\''/, ""); print $1}'
 
 # depois de escolher um canal voce pode simplesmente rodar algo como:
 # vlc [link que voce escolheu]
