65135aea4d062995dc434c094d30cc430aeb9577
museu-do-novo <138774203+museu-do-novo@users.noreply.github.com>
2025-01-24 12:14:12 -0300

    Atualizar o setup.sh

--- setup.sh
+++ setup.sh
@@ -23,7 +23,7 @@ setup_android() {
   # Baixar o VLC APK do link atualizado no F-Droid
   echo "Baixando VLC APK..."
   apk_file=$TMPDIR/vlc.apk
-  
+  wget -O "$apk_file" https://f-droid.org/repo/org.videolan.vlc_13050736.apk
 
   # Instalar o VLC usando termux-open
   echo "Iniciando o instalador do VLC..."
